a25803e2f5accf9359244714e2fc0879109e5345
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-07-29 16:07:51 +0530

    Add files via upload

---
+++ Coding Assignments/Decision_Making/Ckeck_Conditions.js
@@ -0,0 +1,13 @@
+const Check_divisibility = (N) => {
+    let result; 
+  if (N % 6==0 && N % 9==0){
+     result = "Divisible by both";
+  }
+   else{
+     result = "Not Divisible by both";
+   }
+    
+    return result;
+};
+
+// console.log(Check_divisibility(27))
