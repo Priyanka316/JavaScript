25a6441cfb0901356ccdad91e788f34216cbde96
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-08-16 15:23:26 +0530

    Delete Arrays directory

--- Arrays/IncrementNum.js
+++
@@ -1,9 +0,0 @@
-const Inc_Arr = (array,N) => 
-{
-  // let array = [1,2,3,4,5];
-  // let N=5;
-  array.forEach(function(element){
-    console.log(element + 1);
-  })
-
-};
--- Arrays/find_Prod.js
+++
@@ -1,9 +0,0 @@
-const Find_Prod = (array, N) => 
-{
-// let array = [1,2,3,4,5]; 
- let res = 1;
-// let N=5;
-   for(let i=0; i<N; i++)
-      res=res * array[i];
-   return res;
-};
