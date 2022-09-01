4c044cb4bdd780b1dd5510498b1df8ee3fc50442
Priyanka316 <107699187+Priyanka316@users.noreply.github.com>
2022-09-01 10:54:28 +0530

    Add files via upload

---
+++ Assignments/JQueryAssignment/JQuery.js
@@ -0,0 +1,16 @@
+//Q:1. Create a button , on click of which you should be able to hide a paragraph text, create another button onclick of which this paragraph should be visible
+$('button').click(function (){
+    $('p').toggle();    
+})
+
+//Q:2. Create an ajax call, by using this api "https://jsonplaceholder.typicode.com/todos/12" and print the title as the heading of the page
+$('button').click(function(){
+$.ajax({
+    url:"https://jsonplaceholder.typicode.com/todos/5",
+    type: "GET",
+    success: function(data){
+        console.log(data);
+        document.getElementsByTagName("p")[0].innerText = data.title;
+        }
+    })
+})
